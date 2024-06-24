3837eb6ef157fee0b8ed5d940217cd10937b356f
Angelo Gambara <gambara.angelo@gmail.com>
2024-06-24 20:54:11 +0200

    the stupid content tracker

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+        signs = false,
+      },
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
+}
