4ecb9770ba0053627e54c87ba14404a8eb6dc32f
Angelo Gambara <angelo@angelogambara.dev>
2026-04-06 14:28:53 +0200

    chore(bin): big cleanup

--- .local/bin/statusbar/themes/gruvbox-dark.sh
+++ .local/bin/statusbar/themes/gruvbox-dark.sh
@@ -1,108 +1,73 @@
 #!/bin/sh
-# shellcheck disable=SC2034
 
 # ########
 # Colors
 # ########
 
-# Colors from dwm
-gray1="#1d2021"
-gray2="#928374"
-gray3="#a89984"
-gray4="#ebdbb2"
+# 4 grayscale
+gray1=#1d2021
+gray2=#928374
+gray3=#a89984
+gray4=#ebdbb2
 
-# Normal colors
-red="#cc241d"
-green="#98971a"
-yellow="#d79921"
-blue="#458588"
-purple="#b16286"
-cyan="#689d6a"
-orange="#d65d0e"
+# 3 normal colors
+green=#98971a
+yellow=#d79921
+orange=#d65d0e
 
-##########
-# Helper
-##########
-
-die() {
-  echo >&2 "$1"
-  exit 1
-}
-
-command_exists() {
-  command -v "$1" >/dev/null 2>&1 || return 1
-}
-
-check_env() {
-  # Make zsh behave more like POSIX
-  emulate sh 2>/dev/null
-
-  # Prevent root from running
-  [ "$(id -u)" -eq 0 ] && die "Do not run as root"
-
-  # Ensure environment variables are set
-  if [ -z "$STATUSBAR" ]; then
-    die "STATUSBAR is unset"
-  fi
-
-  # Ensure this script is not misused
-  basename=$(basename "$0")
-  if [ "$basename" = "gruvbox-dark.sh" ]; then
-    die "Error: This script must be sourced"
-  fi
-}
+# 2 special cases
+red=#cc241d
+aqua=#689d6a
 
 ##########
 # Main
 ##########
 
-check_env
-
 # Handle statusbar specific logic
 case "$STATUSBAR" in
 dwmblocks)
   # The name of the script sourcing this file
-  case "$basename" in
+  case $(basename "$0") in
   sb-date)
     theme="^c${gray1}^^b${gray4}^"
     ;;
   sb-battery)
-    # shellcheck disable=SC2154
-    if [ "$capacity" -le 15 ]; then
-      theme="^c${gray1}^^b${red}^"
+    if [ -n "$capacity" ]; then
+      if [ "$capacity" -le 15 ]; then
+        theme="^c${gray1}^^b${red}^"
+      else
+        theme="^c${gray1}^^b${yellow}^"
+      fi
     else
-      theme="^c${gray1}^^b${yellow}^"
+      die "Error: capacity is not set"
     fi
-    ;;
-  sb-network)
-    theme="^c${gray1}^^b${orange}^"
     ;;
   sb-volume)
     theme="^c${gray1}^^b${yellow}^"
     ;;
   sb-brightness)
+    theme="^c${gray1}^^b${yellow}^"
+    ;;
+  sb-network)
     theme="^c${gray1}^^b${orange}^"
     ;;
-  sb-mail)
-    theme="^c${gray1}^^b${green}^"
-    ;;
   sb-bluetooth)
-    theme="^c${gray1}^^b${green}^"
+    theme="^c${gray1}^^b${orange}^"
     ;;
   sb-cpu)
     theme="^c${gray1}^^b${orange}^"
     ;;
-  sb-gpu)
-    theme="^c${gray1}^^b${yellow}^"
-    ;;
   sb-memory)
     theme="^c${gray1}^^b${green}^"
     ;;
+  sb-mail)
+    theme="^c${gray1}^^b${green}^"
+    ;;
   sb-swap)
-    theme="^c${gray1}^^b${purple}^"
+    theme="^c${gray1}^^b${aqua}^"
     ;;
   sb-disk)
-    theme="^c${gray1}^^b${purple}^"
+    theme="^c${gray1}^^b${aqua}^"
     ;;
   *)
     die "Error: Unknown status bar module"
@@ -112,47 +77,47 @@ dwmblocks)
   ;;
 somebar)
   # The name of the script sourcing this file
-  case "$basename" in
+  case $(basename "$0") in
   sb-date)
     theme="<span background='$gray4'>"
     ;;
   sb-battery)
-    # shellcheck disable=SC2154
-    if [ "$capacity" -le 15 ]; then
-      theme="<span background='$red'>"
+    if [ -n "$capacity" ]; then
+      if [ "$capacity" -le 15 ]; then
+        theme="<span background='$red'>"
+      else
+        theme="<span background='$yellow'>"
+      fi
     else
-      theme="<span background='$yellow'>"
+      die "Error: capacity is not set"
     fi
-    ;;
-  sb-network)
-    theme="<span background='$orange'>"
     ;;
   sb-volume)
     theme="<span background='$yellow'>"
     ;;
   sb-brightness)
+    theme="<span background='$yellow'>"
+    ;;
+  sb-network)
     theme="<span background='$orange'>"
+    ;;
+  sb-bluetooth)
+    theme="<span background='$orange'>"
+    ;;
+  sb-cpu)
+    theme="<span background='$orange'>"
+    ;;
+  sb-memory)
+    theme="<span background='$green'>"
     ;;
   sb-mail)
     theme="<span background='$green'>"
     ;;
-  sb-bluetooth)
-    theme="<span background='$purple'>"
-    ;;
-  sb-cpu)
-    theme="<span background='$blue'>"
-    ;;
-  sb-gpu)
-    theme="<span background='$blue'>"
-    ;;
-  sb-memory)
-    theme="<span background='$cyan'>"
-    ;;
   sb-swap)
-    theme="<span background='$cyan'>"
+    theme="<span background='$aqua'>"
     ;;
   sb-disk)
-    theme="<span background='$cyan'>"
+    theme="<span background='$aqua'>"
     ;;
   *)
     die "Error: Unknown status bar module"
