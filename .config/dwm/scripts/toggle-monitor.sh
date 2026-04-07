a1a239a076e137456b59f5aa18a8070240e3d756
Angelo Gambara <angelo@angelogambara.dev>
2026-04-08 00:46:16 +0200

    chore: cleanup

--- .config/dwm/scripts/toggle-monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if xrandr --listactivemonitors | grep -q DisplayPort-1-2; then
-  xrandr --output DisplayPort-1-2  --off --right-of DP-2
-else
-  xrandr --output DisplayPort-1-2 --auto --right-of DP-2
-fi
