84904f2dde44f786f77b87c30c4f8119edf62604
Angelo Gambara <angelo@angelogambara.dev>
2026-06-15 19:32:06 +0200

    fix: typo

--- .config/dwl/autostart-parts.sh
+++ .config/dwl/autostart-parts.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Source each file in order
-if [ -d "$XDG_CONFIG_HOME/dwl/wlrinitrc.d" ]; then
-  for f in "$XDG_CONFIG_HOME/dwl/wlrinitrc.d"/?*.sh; do
+if [ -d "$XDG_CONFIG_HOME/dwl/autostart.d" ]; then
+  for f in "$XDG_CONFIG_HOME/dwl/autostart.d"/?*.sh; do
     # shellcheck disable=1090
     [ -x "$f" ] && . "$f"
   done
