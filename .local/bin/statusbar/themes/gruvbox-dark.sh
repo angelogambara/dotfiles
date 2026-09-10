f0ae64497ed8184317786f1a796574c717d316d2
Angelo Gambara <angelogambaradev@noreply.codeberg.org>
2026-09-10 20:06:04 +0200

    wip: move new statusbar to dedicated repo

--- .local/bin/statusbar/themes/gruvbox-dark.sh
+++ .local/bin/statusbar/themes/gruvbox-dark.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Centralized location for setting statusbar theme.
 
 # ########
 # Colors
@@ -23,45 +24,57 @@ aqua=#689d6a
 # Main
 ##########
 
-# Handle statusbar specific logic
-case "$STATUSBAR" in
-dwmblocks)
-  # The name of the script sourcing this file
-  case $(basename "$0") in
-  sb-date)
-    theme="^c${gray1}^^b${gray4}^" ;;
-  sb-battery)
-    if [ -n "$capacity" ]; then
-      if [ "$capacity" -le 15 ]; then
-        theme="^c${gray1}^^b${red}^"
-      else
-        theme="^c${gray1}^^b${yellow}^"
-      fi
-    else
-      die "Error: capacity is not set"
-    fi
-    ;;
-  sb-volume)
-    theme="^c${gray1}^^b${yellow}^" ;;
-  sb-brightness)
-    theme="^c${gray1}^^b${yellow}^" ;;
-  sb-network)
-    theme="^c${gray1}^^b${orange}^" ;;
-  sb-bluetooth)
-    theme="^c${gray1}^^b${orange}^" ;;
-  sb-cpu)
-    theme="^c${gray1}^^b${orange}^" ;;
-  sb-memory)
-    theme="^c${gray1}^^b${green}^" ;;
-  sb-mail)
-    theme="^c${gray1}^^b${green}^" ;;
-  sb-swap)
-    theme="^c${gray1}^^b${aqua}^" ;;
-  sb-disk)
-    theme="^c${gray1}^^b${aqua}^" ;;
-  *)
-    die "Error: Unknown status bar module" ;;
-  esac
-  reset="^b${gray1}^"
+sourced_from="$(basename "$0")"
+
+case "$sourced_from" in
+sb-date)
+  theme="^c${gray1}^^b${gray4}^"
+  ;;
+sb-battery)
+  if [ "${capacity:-0}" -le 15 ]; then
+    theme="^c${gray1}^^b${red}^"
+  else
+    theme="^c${gray1}^^b${yellow}^"
+  fi
+  ;;
+sb-network)
+  theme="^c${gray1}^^b${yellow}^"
+  ;;
+sb-volume)
+  theme="^c${gray1}^^b${yellow}^"
+  ;;
+sb-brightness)
+  theme="^c${gray1}^^b${yellow}^"
+  ;;
+sb-gpu)
+  theme="^c${gray1}^^b${green}^"
+  ;;
+sb-cpu)
+  theme="^c${gray1}^^b${green}^"
+  ;;
+sb-memory)
+  theme="^c${gray1}^^b${green}^"
+  ;;
+sb-cpuall)
+  theme="^c${gray1}^^b${green}^"
+  ;;
+sb-swap)
+  theme="^c${gray1}^^b${green}^"
+  ;;
+sb-bluetooth)
+  theme="^c${gray1}^^b${orange}^"
+  ;;
+sb-mail)
+  theme="^c${gray1}^^b${orange}^"
+  ;;
+sb-disk)
+  theme="^c${gray1}^^b${orange}^"
+  ;;
+sb-count)
+  theme="^c${gray1}^^b${orange}^"
+  ;;
+*)
+  die "Error: Unknown status bar module"
   ;;
 esac
+reset="^b${gray1}^"
